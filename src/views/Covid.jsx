bf076648a8aced7f8d031ecea2455d965186e525
Duong Hanh Minh <minh.duong7438@hcmut.edu.vn>
2023-05-23 22:47:52 +0700

    Handle error fetching data

--- src/views/Covid.jsx
+++ src/views/Covid.jsx
@@ -1,33 +1,44 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const Covid = () =>{
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
     // ComponentDidMount
-    const [users, setUsers] = React.useState([]);
-    useEffect(() => {
-    (async () => {
-        const res = await axios.get("https://reqres.in/api/users?page=2")
-        let data = res && res.data.data ? res.data.data : [];
-        setUsers(data);
-    })();
-    }, []);
-
-    // const [users, setUsers] = React.useState([]);
-    // const f = async () => {
-    //   const res = await fetch("https://reqres.in/api/users/");
-    //   const json = await res.json();
-    //   setUsers(res);
-    // };
-    // React.useEffect(() => {
-    //   f();
+    // useEffect(() => {
+    //     setTimeout(()=>{
+    //         (async () => {         
+    //                 const res = await axios.get("https://reqres.in/api/users?page=2")
+    //                 let data = res && res.data.data ? res.data.data : [];
+    //                 setUsers(data);
+    //                 setLoading(false);
+    //         })();
+    //     },20000)
     // }, []);
-    
+    const fetchData = async () => {
+            try {
+              const response = await axios.get("https://reqres.in/api/users?page=2");
+              const data = response?.data?.data || [];
+              // Simulate delay using setTimeout
+                setTimeout(() => {
+                    setUsers(data);
+                    setLoading(true);
+                    setIsError(false);
+                }, 2000); // 2 seconds delay
+            } catch (error) {
+                setIsError(true);
+                setLoading(false);
+            }
+    };
+    useEffect(() => {
+        fetchData();
+    }, []);  
     //https://ultimatecourses.com/blog/using-async-await-inside-react-use-effect-hook
    
     return(   
     <table id="customers">
-      { console.log('>>> Check data user: ', users)}
         <thead>
             <tr>
                 <th>First Name</th>
@@ -36,16 +47,30 @@ const Covid = () =>{
             </tr>
         </thead>
         <tbody>
-            {users && users.length > 0 
-            && users.map((item) => {
-                return(
+            {loading ? ( 
+                users && users.length > 0 ? ( 
+                users.map((item) => (
                     <tr key={item.id}>
                         <td>{item.first_name}</td>
                         <td>{item.last_name}</td>
                         <td>{item.email}</td>
                     </tr>
+                ))
+            ) : (
+                <tr>
+                    <td colSpan="3">No data available</td>
+                </tr>
                 )
-            } )}
+            ) : (
+                isError ?
+                (<tr>
+                    <td colSpan="3" style={{'textAlign':'center'}}>Something Wrong...!!</td>
+                </tr>
+                ):(
+                <tr>
+                    <td colSpan="3" style={{'textAlign':'center'}}>Loading...</td>
+                </tr>)
+            )}
         </tbody>
     </table>
     )
