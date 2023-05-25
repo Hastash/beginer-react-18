83cc2ba12f2ea0ef286bf80071b3b35b90bd0d1a
Duong Hanh Minh <minh.duong7438@hcmut.edu.vn>
2023-05-25 17:04:02 +0700

    Revert "Reusing Custom Hooks"
    This reverts commit d69650a6f2983318b34ab7684762e8b879690de7.

--- src/views/DetailBlog.jsx
+++ src/views/DetailBlog.jsx
@@ -1,30 +1,14 @@
 import { useParams, useNavigate } from "react-router";
-import useFetch from "../models/fetch";
 const DetailBlog =() => {
     let navigate = useNavigate()
-    let {id} = useParams()
+    let data = useParams()
     const handleBack = () => {
         navigate(-1);
     }
-    const{data: dataBlogDetail, isLoading, isError} = useFetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
-    console.log('Check data detail: ',dataBlogDetail)
     return(
         <>
         <span onClick={handleBack}>&lt;--BACK</span>
-        <div className="blog-detail">
-            {dataBlogDetail && 
-            <>
-                {!isLoading ? "Loading...." :
-                    <>   
-                    <div className="title">
-                        Blog ID: {id} --- {dataBlogDetail.title}</div>
-                    <div className="content">{dataBlogDetail.body}</div>
-                    </>
-                }  
-                
-            </>
-            }
-        </div>
+        <h1>Hello Detail Blogs</h1>
         </>
     )
 }
