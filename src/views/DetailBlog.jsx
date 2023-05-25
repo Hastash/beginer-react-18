bb8fbf861bfe859046ef081faaa372964b0d29c6
Duong Hanh Minh <minh.duong7438@hcmut.edu.vn>
2023-05-25 17:06:30 +0700

    Reuse Custom Hoook - Fetch to display BlogDetail page

--- src/views/DetailBlog.jsx
+++ src/views/DetailBlog.jsx
@@ -1,14 +1,30 @@
 import { useParams, useNavigate } from "react-router";
+import useFetch from "../models/fetch";
 const DetailBlog =() => {
     let navigate = useNavigate()
-    let data = useParams()
+    let {id} = useParams()
     const handleBack = () => {
         navigate(-1);
     }
+    const{data: dataBlogDetail, isLoading, isError} = useFetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+    console.log('Check data detail: ',dataBlogDetail)
     return(
         <>
         <span onClick={handleBack}>&lt;--BACK</span>
-        <h1>Hello Detail Blogs</h1>
+        <div className="blog-detail">
+            {dataBlogDetail && 
+            <>
+                {!isLoading ? "Loading...." :
+                    <>   
+                    <div className="title">
+                        Blog ID: {id} --- {dataBlogDetail.title}</div>
+                    <div className="content">{dataBlogDetail.body}</div>
+                    </>
+                }  
+                
+            </>
+            }
+        </div>
         </>
     )
 }
